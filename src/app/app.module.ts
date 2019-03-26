8de3d9b8d28556b012fbf2934f4d3e2430d8f303
kumarandena <kumarandena@gmail.com>
2019-03-27 01:20:40 +0530

    added material & animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppMaterialModule } from './app.material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
+  declarations: [ 
     AppComponent
   ],
   imports: [
     BrowserModule,
+    AppMaterialModule,
     AppRoutingModule
   ],
   providers: [],
