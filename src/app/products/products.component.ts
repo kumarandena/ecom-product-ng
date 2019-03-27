72737659062317037f99212c13227dd21cca8037
kumarandena <kumarandena@gmail.com>
2019-03-27 22:39:31 +0530

    added snackbar, edit & delete logic

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { EditDialog } from './edit/edit.component';
+import { MatSnackBar } from '@angular/material';
 
 export interface productData {
   name: string;
@@ -26,20 +27,25 @@ export class ProductsComponent implements OnInit {
     { id: '', name: "Mi", isSmart: true, description: "Enjoy television viewing like never before with this 108-cm (43) Full HD LED TV from Mi. Apart from featuring 1 GB of RAM and 8 GB of storage space, this TV also comes with cinematic sound quality, a rich range of colours, and multiple connectivity options. Also, with PatchWall and Google Voice Search, navigating and finding content to watch is simple and convenient.", height: 24.5, width: 43.6 },
   ];
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, private snackBar: MatSnackBar) { }
 
   openDialog(index: string, product: productData): void {
     console.log("index : ", index, product)
     // let dat = this.products.filter((product) => product.id === id)[0];
     // console.log("filtered data ", dat)
-    product.isSmart = product.isSmart ? 'Yes' : 'No';
     const dialogRef = this.dialog.open(EditDialog, {
       width: '250px',
-      data: product
+      data: { n: product.name, i: product.isSmart ? 'Yes' : 'No', d: product.description, w: product.width, h: product.height }
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed', result);
+      this.products[index].description = result.d;
+      this.products[index].isSmart = result.i == "Yes" ? true : false;
+      this.products[index].width = result.w;
+      this.products[index].height = result.h;
+      this.snackBar.open(result.n + ' updated successfully!', '', {
+        duration: 2500,
+      });
     });
   }
 
@@ -50,8 +56,11 @@ export class ProductsComponent implements OnInit {
     // })
   }
 
-  onDelete(): void {
-    console.log("complete data", this.products)
+  onDelete(i: number, name: string): void {
+    this.products.splice(i, 1);
+    this.snackBar.open(name + ' deleted successfully!', '', {
+      duration: 2000,
+    });
   }
 }
 
